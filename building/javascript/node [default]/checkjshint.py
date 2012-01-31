9d953ab7dd1c7688f01637a91f208b5e0bd8434a
Alex Gordon <anything@fileability.net>
2012-01-31 03:00:43 +0000

    CHnages to jshint

--- building/javascript/node [default]/checkjshint.py
+++ building/javascript/node [default]/checkjshint.py
@@ -3,7 +3,24 @@ import re
 import json
 
 def invoke():
-    output = str(commands.getoutput("sh env/jsc.sh $CHOC_FILE \"laxbreak:true,laxcomma:true\""))
+    true_options = [
+        "laxcomma",
+        "laxbreak",
+        "eqnull",
+        
+        "es5",
+        "esnext",
+        "iterator",
+        "multistr",
+        "scripturl",
+        "smarttabs",
+        "supernew",
+
+        "asi",
+    ]
+    options_arg = ','.join([opt + ':true' for opt in true_options])
+    #print "sh env/jsc.sh $CHOC_FILE \"%s\"" % options_arg
+    output = str(commands.getoutput("sh env/jsc.sh $CHOC_FILE \"%s\"" % options_arg))
     parse(output)
 
 def parse(output):
@@ -16,12 +33,19 @@ def parse_match(match):
     line = int(match[1])
     message = match[0]
     column = match[2]
+    
+    t = 'warning'
+    if 'Expected' in message or 'Unmatched' in message:
+        t = 'error'
+    if 'Expected a conditional expression and instead saw an assignment' in message:
+        t = 'warning'
+    t
     return {
         'path': '',
         'line': line,
         'message': message,
         'column': column,
-        'type': 'error',
+        'type': t,
     }
 
 def display(output):
