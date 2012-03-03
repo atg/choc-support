7f9644560ef6a563a51ea0128b5a5da1991bf37a
Alex Gordon <anything@fileability.net>
2012-03-03 19:06:21 +0000

    Some change to build.sh

--- build.sh
+++ build.sh
@@ -4,5 +4,7 @@
 find . -name '*.coffee' | xargs coffee -b -c
 
 # Run the json compilation scripts
-
+cd docs && python compile.py && cd ..
+cd completions && python compile.py && cd ..
+cd SPLDB && python compiler.py && cd ..
 
