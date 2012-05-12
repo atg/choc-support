5a5f05a6fd4110661fa748b7e9a414968d545e47
Ben Stolovitz <ben@stolovitz.com>
2012-05-12 15:16:52 -0400

    Initial `build.sh`
    Not robust at all, but something.

---
+++ building/actionscript/build.sh
@@ -0,0 +1,29 @@
+cd $CHOC_FILE_DIR;
+
+# By convention, AS3 builds to `bin/`
+# But when in Rome, I guess:
+mkdir -p "$CHOC_BUILD_DIR"; 
+
+if [ ! mxmlc ]; then
+	echo "Please add mxmlc to your $PATH"
+	# Untested on other systems.
+fi
+
+# Thank you, Pini: http://bit.ly/w1VOPU
+relativePath () {
+	source=$1
+	target=$2
+
+	common_part=$source
+	back=
+	while [ "${target#$common_part}" = "${target}" ]; do
+	  common_part=$(dirname $common_part)
+	  back="../${back}"
+	done
+
+	echo ${back}${target#$common_part/}
+}
+
+BUILD_PATH=`relativePath $CHOC_FILE_DIR $CHOC_BUILD_DIR`;
+
+mxmlc "$CHOC_FILE" -sp "$CHOC_FILE_DIR" -o "$BUILD_PATH/Main.swf" -static-link-runtime-shared-libraries=true;
