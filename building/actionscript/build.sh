11e0fda12d9389b7966e91cfa22fbf723731a11f
Ben Stolovitz <ben@stolovitz.com>
2012-05-12 16:34:12 -0400

    Refactor.

--- building/actionscript/build.sh
+++ building/actionscript/build.sh
@@ -1,29 +1,12 @@
-cd $CHOC_FILE_DIR;
-
 # By convention, AS3 builds to `bin/`
 # But when in Rome, I guess:
 mkdir -p "$CHOC_BUILD_DIR"; 
+
+cd $CHOC_BUILD_DIR;
 
 if [ ! mxmlc ]; then
 	echo "Please add mxmlc to your $PATH"
 	# Untested on other systems.
 fi
 
-# Thank you, Pini: http://bit.ly/w1VOPU
-relativePath () {
-	source=$1
-	target=$2
-
-	common_part=$source
-	back=
-	while [ "${target#$common_part}" = "${target}" ]; do
-	  common_part=$(dirname $common_part)
-	  back="../${back}"
-	done
-
-	echo ${back}${target#$common_part/}
-}
-
-BUILD_PATH=`relativePath $CHOC_FILE_DIR $CHOC_BUILD_DIR`;
-
-mxmlc "$CHOC_FILE" -sp "$CHOC_FILE_DIR" -o "$BUILD_PATH/$CHOC_FILENAME_NOEXT.swf" -static-link-runtime-shared-libraries=true;
+mxmlc "$CHOC_FILE" -sp "$CHOC_PROJECT_DIR/src" -o "$CHOC_FILENAME_NOEXT.swf" -static-link-runtime-shared-libraries=true;
