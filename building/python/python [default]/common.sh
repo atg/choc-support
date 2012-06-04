21ad3dabbed60056746a0432cc13495b2dfe309d
Alice Bevan-McGregor <alice@gothcandy.com>
2012-06-04 13:38:11 -0400

    Bug fix; forgot an x in an empty variable check.

--- building/python/python [default]/common.sh
+++ building/python/python [default]/common.sh
@@ -5,25 +5,25 @@ SYSTEM_PYTHON=$(which python)
 if [[ "x$CHOC_PROJECT_DIR" != "x" ]]; then
     echo -e ">>> Project in:\n>>>\t$CHOC_PROJECT_DIR"
     echo -e ">>> File in:\n>>>\t$CHOC_FILE_DIR"
-    
+
     cd "$CHOC_FILE_DIR"
-    
+
     # Determine if we're within a full Python package.
     if [[ -f setup.py ]]; then
         SETUP="$PWD/setup.py"
     else
         while [[ "$PWD" != "$CHOC_PROJECT_DIR" ]]; do
             cd .. || break
-            
+
             if [[ -f setup.py ]]; then
                 SETUP="$PWD/setup.py"
                 break
             fi
         done
     fi
-    
+
     cd "$CHOC_FILE_DIR"
-    
+
     # Activate a virtual environment if we can find it.
     if [[ -f bin/activate ]]; then
         source bin/activate
@@ -37,9 +37,9 @@ if [[ "x$CHOC_PROJECT_DIR" != "x" ]]; then
             fi
         done
     fi
-    
+
     cd "$CHOC_PROJECT_DIR"
-    
+
     if [[ "x$VIRTUAL_ENV" == 'x' && -e "bin" ]]; then
         echo ">>> Adding project bin folder to path."
         export PATH="$CHOC_PROJECT_DIR/bin:$PATH"
@@ -49,4 +49,4 @@ fi
 # Emit some potentially useful information about the Python we're checking against.
 echo -e ">>> $VERB against $(python -V 2>&1) from:\n>>>\t$(which python)"
 
-[[ "x$SETUP" != "" && "x$VIRTUAL_ENV" != 'x' ]] && echo -e ">>> Registering package with virtualenv:\n>>>\tpython $SETUP develop" && python $SETUP develop > /dev/null
+[[ "x$SETUP" != "x" && "x$VIRTUAL_ENV" != 'x' ]] && echo -e ">>> Registering package with virtualenv:\n>>>\tpython $SETUP develop" && python $SETUP develop > /dev/null
