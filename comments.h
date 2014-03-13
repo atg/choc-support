416fd7d233f361189f851c2f8ce2b8e44c1292cf
Alex Gordon <alextgordon@gmail.com>
2014-03-13 02:49:38 +0000

    Comments!

---
+++ comments.h
@@ -0,0 +1,111 @@
+if (MATCH_LANG(@"c") || MATCH_LANG(@"c++") || MATCH_LANG(@"objc") || MATCH_LANG(@"objc++") || MATCH_LANG(@"actionscript") || MATCH_LANG(@"csharp") || MATCH_LANG(@"go") || MATCH_LANG(@"java") || MATCH_LANG(@"js") || MATCH_LANG(@"lemon") || MATCH_LANG(@"objj") || MATCH_LANG(@"pike") || MATCH_LANG(@"scala") || MATCH_LANG(@"scss") || MATCH_LANG(@"verilog")) {
+    MATCH_LINE_COMMENT(@"//");
+    MATCH_BLOCK_COMMENT(@"/*", @"*/");
+}
+if (MATCH_LANG(@"ada")) {
+    MATCH_LINE_COMMENT(@"--");
+}
+if (MATCH_LANG(@"applescript")) {
+    MATCH_LINE_COMMENT(@"--");
+    MATCH_BLOCK_COMMENT(@"(*", @"*)");
+}
+if (MATCH_LANG(@"asm") || MATCH_LANG(@"clojure") || MATCH_LANG(@"lisp")) {
+    MATCH_LINE_COMMENT(@";");
+}
+if (MATCH_LANG(@"coffeescript")) {
+    MATCH_LINE_COMMENT(@"#");
+    MATCH_BLOCK_COMMENT(@"///", @"///");
+}
+if (MATCH_LANG(@"css")) {
+    MATCH_BLOCK_COMMENT(@"/*", @"*/");
+}
+if (MATCH_LANG(@"d")) {
+    MATCH_LINE_COMMENT(@"//");
+    MATCH_BLOCK_COMMENT(@"/*", @"*/");
+    MATCH_BLOCK_COMMENT(@"/+", @"+/");
+}
+if (MATCH_LANG(@"erlang") || MATCH_LANG(@"latex") || MATCH_LANG(@"tex") || MATCH_LANG(@"postscript")) {
+    MATCH_LINE_COMMENT(@"%");
+}
+if (MATCH_LANG(@"fortran")) {
+    MATCH_LINE_COMMENT(@"!");
+}
+if (MATCH_LANG(@"fsharp")) {
+    MATCH_LINE_COMMENT(@"//");
+    MATCH_BLOCK_COMMENT(@"(*", @"*)");
+}
+if (MATCH_LANG(@"haml")) {
+    MATCH_LINE_COMMENT(@"-#");
+}
+if (MATCH_LANG(@"haskell")) {
+    MATCH_LINE_COMMENT(@"--");
+    MATCH_BLOCK_COMMENT(@"{-", @"-}");
+}
+if (MATCH_LANG(@"html") || MATCH_LANG(@"textile") || MATCH_LANG(@"xml") || MATCH_LANG(@"xslt")) {
+    MATCH_BLOCK_COMMENT(@"<!--", @"-->");
+}
+if (MATCH_LANG(@"io")) {
+    MATCH_LINE_COMMENT(@"#");
+    MATCH_BLOCK_COMMENT(@"/*", @"*/");
+}
+if (MATCH_LANG(@"lua")) {
+    MATCH_LINE_COMMENT(@"--");
+    MATCH_BLOCK_COMMENT(@"--[[", @"--]]");
+}
+if (MATCH_LANG(@"ocaml")) {
+    MATCH_BLOCK_COMMENT(@"(*", @"*)");
+}
+if (MATCH_LANG(@"oz") || MATCH_LANG(@"prolog")) {
+    MATCH_LINE_COMMENT(@"%");
+    MATCH_BLOCK_COMMENT(@"/*", @"*/");
+}
+if (MATCH_LANG(@"pascal")) {
+    MATCH_LINE_COMMENT(@"//");
+    MATCH_BLOCK_COMMENT(@"{", @"}");
+}
+if (MATCH_LANG(@"perl") || MATCH_LANG(@"r") || MATCH_LANG(@"ragel") || MATCH_LANG(@"shell") || MATCH_LANG(@"tcl")) {
+    MATCH_LINE_COMMENT(@"#");
+}
+if (MATCH_LANG(@"php")) {
+    MATCH_LINE_COMMENT(@"//");
+    MATCH_LINE_COMMENT(@"#");
+    MATCH_BLOCK_COMMENT(@"/*", @"*/");
+}
+if (MATCH_LANG(@"python")) {
+    MATCH_LINE_COMMENT(@"#");
+    MATCH_BLOCK_COMMENT(@"\"\"\"", @"\"\"\"");
+}
+if (MATCH_LANG(@"rexx")) {
+    MATCH_BLOCK_COMMENT(@"/*", @"*/");
+}
+if (MATCH_LANG(@"ruby")) {
+    MATCH_LINE_COMMENT(@"#");
+    MATCH_BLOCK_COMMENT(@"=begin", @"=end");
+}
+if (MATCH_LANG(@"sass")) {
+    MATCH_LINE_COMMENT(@"//");
+}
+if (MATCH_LANG(@"scheme")) {
+    MATCH_LINE_COMMENT(@";");
+    MATCH_BLOCK_COMMENT(@"#|", @"|#");
+}
+if (MATCH_LANG(@"selfml")) {
+    MATCH_LINE_COMMENT(@"#");
+    MATCH_BLOCK_COMMENT(@"{#", @"#}");
+}
+if (MATCH_LANG(@"smalltalk")) {
+    MATCH_BLOCK_COMMENT(@"\"", @"\"");
+}
+if (MATCH_LANG(@"sql")) {
+    MATCH_LINE_COMMENT(@"--");
+    MATCH_BLOCK_COMMENT(@"/*", @"*/");
+}
+if (MATCH_LANG(@"vhdl")) {
+    MATCH_LINE_COMMENT(@"--");
+}
+if (MATCH_LANG(@"furrow")) {
+    MATCH_LINE_COMMENT(@"--");
+}
+if (MATCH_LANG(@"yaml")) {
+    MATCH_LINE_COMMENT(@"#");
+}
---
+++ comments_gen.py
@@ -0,0 +1,60 @@
+from pprint import pprint
+
+def comments_gen():
+    with open('comments.txt', 'rb') as f:
+        current_group = None
+        for line in f:
+            is_data = line.startswith(' ')
+            line = line.strip()
+            
+            if not line:
+                continue
+            
+            if is_data:
+                items = line.split()
+                if len(items) == 1:
+                    # Line comment
+                    current_group['line_comments'].append(items[0])
+                else:
+                    # Block comment
+                    current_group['block_comments'].append(items)
+            else:
+                if current_group:
+                    yield current_group
+                
+                current_group = {
+                    'languages': line.split(),
+                    'line_comments': [],
+                    'block_comments': [],
+                }
+        
+        if current_group:
+            yield current_group
+
+def escape(s):
+    return s.replace('\\', '\\\\').replace('"', r'\"')
+
+def matcher(lang):
+    return 'MATCH_LANG(@"%s")' % lang
+
+def comments_header():
+    for group in comments_gen():
+        matchers = ' || '.join(map(matcher, group['languages']))
+        yield 'if (%s) {' % matchers
+        
+        for lc in group['line_comments']:
+            yield '    MATCH_LINE_COMMENT(@"%s");' % escape(lc)
+        for bcs, bce in group['block_comments']:
+            yield '    MATCH_BLOCK_COMMENT(@"%s", @"%s");' % (escape(bcs), escape(bce))
+        
+        yield '}'
+            
+    #pprint(list(comments_gen()))
+
+def main():
+    with open('comments.h', 'wb') as f:
+        for line in comments_header():
+            f.write(line + '\n')
+
+if __name__ == '__main__':
+    main()
