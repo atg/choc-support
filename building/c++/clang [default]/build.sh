5994179f43bc31ffa5f5e159cea8b26eb6bb01f8
Alex Gordon <alextgordon@gmail.com>
2014-12-12 04:02:10 +0000

    Rust snippets, lua diagnostics, clang fixes

--- building/c++/clang [default]/build.sh
+++ building/c++/clang [default]/build.sh
@@ -1,1 +1,1 @@
-mkdir -p "$CHOC_BUILD_DIR"; clang++ -x c++ -Os -o "$CHOC_BUILD_DESTINATION" "$CHOC_FILE"
+mkdir -p "$CHOC_BUILD_DIR"; clang++ -std=c++11 -stdlib=libc++ -x c++ -Os -o "$CHOC_BUILD_DESTINATION" "$CHOC_FILE"
---
+++ building/lua/lua [default]/check_lua.py
@@ -0,0 +1,31 @@
+import commands
+import re
+import json
+
+def invoke():
+    output = str(commands.getoutput("""$SHELL -cl 'luac -p "$CHOC_FILE"'"""))
+    parse(output)
+
+def parse(output):
+    errormessage = r'^luac\: ([^\n:]+)\:(\d+)\: ([^\n]+)'
+    fullregex = re.compile(errormessage, re.MULTILINE)
+    matches = re.findall(fullregex, output)
+    display(map(parse_match, matches))
+
+def parse_match(match):
+    # path = match[0]
+    line = int(match[1])
+    message = match[2]
+    
+    return {
+        'path': '',
+        'line': line,
+        'message': message,
+        'column': -1,
+        'type': 'warning' if 'warning' in message else 'error',
+    }
+
+def display(output):
+    print json.dumps(output)
+
+invoke()
