f224a8f16bb5002b40d6a7b8f8b22bd040cdeb43
Alex Gordon <alextgordon@gmail.com>
2015-03-25 09:53:39 +0000

    ...

--- building/rust/rustc [default]/build.sh
+++ building/rust/rustc [default]/build.sh
@@ -1,1 +1,7 @@
-mkdir -p "$CHOC_BUILD_DIR"; rustc -O -o "$CHOC_BUILD_DESTINATION" "$CHOC_FILE"
+if [ -f "$CHOC_FILE_DIR/Cargo.toml"] then
+    cd "$CHOC_FILE_DIR" && cargo build
+elif [ -f "$CHOC_PROJECT_DIR/Cargo.toml"] then
+    cd "$CHOC_PROJECT_DIR" && cargo build
+else
+    mkdir -p "$CHOC_BUILD_DIR"; rustc -O -o "$CHOC_BUILD_DESTINATION" "$CHOC_FILE"
+fi
--- building/rust/rustc [default]/run.sh
+++ building/rust/rustc [default]/run.sh
@@ -1,1 +1,7 @@
-bash build.sh && cd "$CHOC_RUN_DIRECTORY" && exec "$CHOC_BUILD_DESTINATION"
+if [ -f "$CHOC_FILE_DIR/Cargo.toml"] then
+    cd "$CHOC_FILE_DIR" && cargo run
+elif [ -f "$CHOC_PROJECT_DIR/Cargo.toml"] then
+    cd "$CHOC_PROJECT_DIR" && cargo run
+else
+    bash build.sh && cd "$CHOC_RUN_DIRECTORY" && exec "$CHOC_BUILD_DESTINATION"
+fi
